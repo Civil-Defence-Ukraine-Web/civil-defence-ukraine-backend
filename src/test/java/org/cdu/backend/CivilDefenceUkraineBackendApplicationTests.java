f8c3fb8ec3b079bb4c65dea21647d1f119a51aac
Vlad <vladyslav.herasymenkoo@gmail.com>
2024-09-26 22:30:00 +0200

    Initial commit. Empty project, required dependencies and checkstyle

---
+++ src/test/java/org/cdu/backend/CivilDefenceUkraineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cdu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilDefenceUkraineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
