82590bba0094cf662688d82c31bfee05c69df893
Vlad <vladyslav.herasymenkoo@gmail.com>
2024-09-28 21:29:29 +0200

    Fixed checkstyle

--- src/main/java/org/cdu/backend/dto/news/NewsUpdateRequestDto.java
+++ src/main/java/org/cdu/backend/dto/news/NewsUpdateRequestDto.java
@@ -1,6 +1,5 @@
 package org.cdu.backend.dto.news;
 
-import jakarta.validation.constraints.NotBlank;
 import org.cdu.backend.model.News;
 
 public record NewsUpdateRequestDto(String title, String text,
