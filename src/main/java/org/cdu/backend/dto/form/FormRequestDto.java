c740f3f548bcda9b777a4cac25e55ec46f8c4f41
Vlad <vladyslav.herasymenkoo@gmail.com>
2024-10-01 23:25:11 +0200

    added service and controller. test failures...

--- src/main/java/org/cdu/backend/dto/form/FormRequestDto.java
+++ src/main/java/org/cdu/backend/dto/form/FormRequestDto.java
@@ -1,4 +1,8 @@
 package org.cdu.backend.dto.form;
 
-public record FormRequestDto() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record FormRequestDto(@NotBlank @Email String email, @NotBlank String subject,
+                             @NotBlank String message, @NotBlank boolean isVolunteer) {
 }
