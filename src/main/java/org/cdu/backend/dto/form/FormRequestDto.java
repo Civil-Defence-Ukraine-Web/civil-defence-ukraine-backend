ac09907c9a0377f2d811e629e30025b2b47f315d
Vlad <vladyslav.herasymenkoo@gmail.com>
2024-10-20 12:51:52 +0200

    Changed CORS configure, so now connection allowed from all domains

--- src/main/java/org/cdu/backend/dto/form/FormRequestDto.java
+++ src/main/java/org/cdu/backend/dto/form/FormRequestDto.java
@@ -2,7 +2,8 @@ package org.cdu.backend.dto.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record FormRequestDto(@NotBlank @Email String email, @NotBlank String subject,
-                             @NotBlank String message, @NotBlank boolean isVolunteer) {
+                             @NotBlank String message, @NotNull boolean isVolunteer) {
 }
